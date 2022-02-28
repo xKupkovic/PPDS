4a0ead299fdc6a70d110815acd69289e9c6adba0
xKupkovic <xKupkovic@stuba.sk>
2022-02-28 20:21:47 +0100

    feat: implement semaphore

--- Uloha3.py
+++ Uloha3.py
@@ -1,23 +1,27 @@
-from fei.ppds import Mutex,print,Semaphor,Thread
+from fei.ppds import Mutex,print,Semaphore,Thread
 
 def get_next_fibonacci(i):
-    global nums
     return nums[i-2]+nums[i-1]
     pass
 
-def fibonacci_cycle():
-    count = 2
-    while True:
-        
+def fibonacci_cycle(thread_id):
+    global t
+    nums.append(get_next_fibonacci(thread_id+2))
     pass
     
 THREADS_NUMBER = 5
+
+t = Semaphore(0)
+
+calculated = 0
 
 nums = []
 
 nums.append(0)
 nums.append(1)
 
-threads = [ Thread(fibonacci_cycle) for i in range(THREADS_NUMBER)]
+threads = [ Thread(fibonacci_cycle,i) for i in range(THREADS_NUMBER)]
 [t.join() for t in threads]
+
+print(nums)
 
