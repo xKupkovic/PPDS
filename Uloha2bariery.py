d6a722806fb687ab32c09f6cdab231b0b936975e
xKupkovic <xKupkovic@stuba.sk>
2022-02-28 18:54:27 +0100

    feat: add simple barrier class

--- Uloha2bariery.py
+++ Uloha2bariery.py
@@ -1,8 +1,26 @@
 from time import sleep
 from random import randint
-from fei.ppds import Thread, Mutex, Semaphore
+from fei.ppds import Thread, Mutex, Event
 from fei.ppds import print
+
+class SimpleBarrier:
+    def __init__(self, N):
+        self.N = N
+        self.C = 0
+        self.M = Mutex()
+        self.T = Event()
+        pass
  
+    def wait(self):
+        self.M.lock()
+        self.C += 1
+        if(self.C==self.N):
+            self.C = 0
+            self.T.signal()
+        self.M.unlock()
+        self.T.wait()
+        self.T.clear()
+        pass
  
 def rendezvous(thread_name):
     sleep(randint(1,10)/10)
@@ -19,6 +37,8 @@ def barrier_example(thread_name):
         rendezvous(thread_name)
         ko(thread_name)
 
+sb1 = SimpleBarrier(5)
+sb2 = SimpleBarrier(5)
  
 threads = list()
 for i in range(5):
