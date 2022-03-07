34fa64b12a5fdb677b61d8f291cd09b4824627b5
xKupkovic <xKupkovic@stuba.sk>
2022-03-07 21:40:55 +0100

    feat: set optimal variables

--- P-C.py
+++ P-C.py
@@ -35,44 +35,15 @@ def consumer(shared):
     pass
 
 def main_loop():
-    plt_x=[]
-    plt_y=[]
-    avg_produced = []
-    avg_consumed = []
-    for x in range(1,11):
-        for y in range(1,11):
-            plt_x.append(x)
-            plt_y.append(y)
-            produced = []
-            consumed = []
-            for i in range(10):
-                s = Shared(10)
-                c = [Thread(consumer, s) for j in range(x)]
-                p = [Thread(producer, s) for j in range(y)]
-                sleep(0.1)
-                s.finished = True
-                produced.append(s.produced)
-                consumed.append(s.consumed)
-                s.free.signal(100)
-                s.items.signal(100)
-                [t.join() for t in c+p]
-            avg_produced.append(sum(produced)/10)
-            avg_consumed.append(sum(consumed)/10)
+    s = Shared(10)
+    c = [Thread(consumer, s) for j in range(6)]
+    p = [Thread(producer, s) for j in range(4)]
+    sleep(0.1)
+    s.finished = True
+    s.free.signal(100)
+    s.items.signal(100)
+    [t.join() for t in c+p]
 
-    fig = plt.figure(1)
-    plt.title('average produced')
-    plt.xlabel('consumer')
-    plt.ylabel('producer')
-    ax= plt.axes(projection = '3d')
-    ax.scatter(plt_x, plt_y, avg_produced, c=avg_produced, cmap='viridis', linewidth=2)
-    figb = plt.figure(2)
-    plt.title('average consumed')
-    plt.xlabel('consumer')
-    plt.ylabel('producer')
-    ay= plt.axes(projection = '3d')
-    ay.scatter(plt_x, plt_y, avg_consumed, c=avg_produced, cmap='viridis', linewidth=2)
-    fig.show()
-    figb.show()
 
 main_loop()     
         
