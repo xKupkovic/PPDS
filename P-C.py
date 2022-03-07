5f71edfdf0e50bff5deea6a637733462d68128b0
xKupkovic <xKupkovic@stuba.sk>
2022-03-07 20:14:35 +0100

    feat: add shared class

--- P-C.py
+++ P-C.py
@@ -1,3 +1,10 @@
 from time import sleep
 from random import randint
 from fei.ppds import Mutex, Semaphore, Thread, print
+
+class Shared:
+    def __init__(self, N):
+        self.finished = False
+        self.mutex = Mutex()
+        self.free = Semaphore(N)
+        self.items = Semaphore(0)
