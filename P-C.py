e5f841ab6a46b56d22767272085b67cd85145618
xKupkovic <xKupkovic@stuba.sk>
2022-03-07 21:39:21 +0100

    feat: add plotting

--- P-C.py
+++ P-C.py
@@ -1,6 +1,8 @@
 from time import sleep
 from random import randint
 from fei.ppds import Mutex, Semaphore, Thread, print
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
 
 class Shared:
     def __init__(self, N):
@@ -33,10 +35,14 @@ def consumer(shared):
     pass
 
 def main_loop():
+    plt_x=[]
+    plt_y=[]
     avg_produced = []
     avg_consumed = []
-    for x in range(1,4):
-        for y in range(1,4):
+    for x in range(1,11):
+        for y in range(1,11):
+            plt_x.append(x)
+            plt_y.append(y)
             produced = []
             consumed = []
             for i in range(10):
@@ -50,8 +56,23 @@ def main_loop():
                 s.free.signal(100)
                 s.items.signal(100)
                 [t.join() for t in c+p]
-            print(sum(produced)/10)
-            print(sum(consumed)/10)
+            avg_produced.append(sum(produced)/10)
+            avg_consumed.append(sum(consumed)/10)
+
+    fig = plt.figure(1)
+    plt.title('average produced')
+    plt.xlabel('consumer')
+    plt.ylabel('producer')
+    ax= plt.axes(projection = '3d')
+    ax.scatter(plt_x, plt_y, avg_produced, c=avg_produced, cmap='viridis', linewidth=2)
+    figb = plt.figure(2)
+    plt.title('average consumed')
+    plt.xlabel('consumer')
+    plt.ylabel('producer')
+    ay= plt.axes(projection = '3d')
+    ay.scatter(plt_x, plt_y, avg_consumed, c=avg_produced, cmap='viridis', linewidth=2)
+    fig.show()
+    figb.show()
 
 main_loop()     
         
