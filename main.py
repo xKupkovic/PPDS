4f483d3ca7a0032e427e724adb62696608728ffe
xkupkovic <xkupkovic@stuba.sk>
2022-03-14 23:12:51 +0100

    feat: add light switch class

--- main.py
+++ main.py
@@ -7,3 +7,25 @@ class SharedData:
         self.ad = Semaphore(1)
         self.ts = Semaphore(1)
         self.vd = Event()
+
+class LS:
+    def __init__(self):
+        self.m = Mutex()
+        self.c = 0
+
+    def lock(self, sem):
+        self.m.lock()
+        self.c += 1
+        if self.c == 1:
+            sem.wait()
+
+        self.m.unlock()
+        return self.c
+
+    def unlock(self, sem):
+        self.m.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            sem.signal()
+
+        self.mutex.unlock()
