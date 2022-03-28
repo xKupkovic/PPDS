615d4bc2c176b35c6b4083918821c646fab875bc
xkupkovic <xkupkovic@stuba.sk>
2022-03-28 22:42:54 +0200

    feat: add barrier back

--- main.py
+++ main.py
@@ -29,6 +29,39 @@ class Shared:
         self.unboarded = Event()
         pass
 
+class SimpleBarrier:
+    def __init__(self, n):
+        """
+        Initalize Simple barrier with number of iterations
+        :param n: number of iterations
+        """
+        self.n = n
+        self.count = 0
+        self.mutex = Mutex()
+        self.barrier = Semaphore(0)
+
+    def wait(self, callback=None, callback_params=[], each=None, last=None):
+        """
+        Add waiting thread to barrier
+        :param callback_params: parameters for function on last
+        :param callback: function to call on last
+        :param each: string printed for each
+        :param last: string printed for last
+        :return: NONE
+        """
+        self.mutex.lock()
+        self.count += 1
+        if each:
+            print(each)
+        if self.count == self.n:
+            if last:
+                print(last)
+            if callback is not None:
+                callback(*callback_params)
+            self.count = 0
+            self.barrier.signal(self.n)
+        self.mutex.unlock()
+        self.barrier.wait()
 
 
 def load(id):
