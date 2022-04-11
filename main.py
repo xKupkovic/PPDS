89727ddde377389d0a812dabf72727978ec86a42
xkupkovic <xkupkovic@stuba.sk>
2022-04-11 20:22:12 +0200

    feat: add program skeleton

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ Date: 11.4.2022
 
 Description: Program runs multiple generators and switches between them.
 """
+
+def generator(count,i):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
