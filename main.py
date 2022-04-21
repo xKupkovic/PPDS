cea0e985c41f80c0a2d61f883f60d23948a690b4
xkupkovic <xkupkovic@stuba.sk>
2022-04-21 17:56:29 +0200

    feat: add program skeleton

--- main.py
+++ main.py
@@ -2,9 +2,24 @@
 Author: Jakub Kupkovic
 Date: 21.4.2022
 
-Description: Program runs sync and async tasks and
+Description: Program runs sync and async tasks and compares speeds
 """
 
+async def async_task(name,work_queue):
+    """
+    Async task that sleeps for given time
+    :param name: name of worker
+    :param work_queue: tasks to do
+    """
+    pass
+
+def sync_task(name,work_queue):
+    """
+    Synchronous task that sleeps for given time
+    :param name: name of worker
+    :param work_queue: tasks to do
+    """
+    pass
 
 def main():
     pass
