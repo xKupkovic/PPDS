32202a5df771a8d9ed1f23cb4ce0d855a989766a
xkupkovic <xkupkovic@stuba.sk>
2022-04-21 18:02:30 +0200

    feat: implement async task

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ Date: 21.4.2022
 Description: Program runs sync and async tasks and compares speeds
 """
 
+from time import sleep
+
 async def async_task(name,work_queue):
     """
     Async task that sleeps for given time
     :param name: name of worker
     :param work_queue: tasks to do
     """
+    while not work_queue.empty():
+        time = work_queue.get()
+        print(f"Async task {name} starting")
+        sleep(time)
+        print(f"Async task {name} finished")
     pass
 
 def sync_task(name,work_queue):
