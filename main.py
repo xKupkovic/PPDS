f370bc5b23d44893ab0abba629d82330ca8ff041
Jakub Kupkovic <jakub.kupkovicc@gmail.com>
2022-04-25 17:16:35 +0200

    feat: add generate random bit matrix function

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 Author: Jakub Kupkovic
 Date: 25.4.2022
 
-Description: Implementation of matrix number operation on CUDA
+Description: Bruteforce search for matrix solution
 """
 from numba import cuda
+import math
+
+def generate_random_bit_matrix(n,m):
+    """
+    Generates random bit matrix
+    :param n: number of columns
+    :param m: number of rows
+    :return: 2d (n*m) array of bits
+    """
+    return [[i for i in range(n)] for j in range(m)]
+
 
 def main():
     print(cuda.gpus)
